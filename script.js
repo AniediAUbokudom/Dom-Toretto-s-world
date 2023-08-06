f89050e3ac9269dd32a1c14d48d3be8830dcba29
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-08-05 21:18:44 -0400

    DOM's WORLD

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let quotes = [
   `I live my life a quarter mile at a time`,
   `I said a ten-second car, not a ten-minute car`,
@@ -17,32 +18,72 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  const mainTitle = document.querySelector('#main-title');
+  mainTitle.textContent = "You're in Toretto's World!";
+  
 
   // Part 2
-
+this.body.style.backgroundColor = "yellow";
 
   // Part 3
+let dsFavThings = document.getElementById('favorite-things');
+dsFavThings.removeChild(dsFavThings.lastElementChild);
 
 
   // Part 4
-
+  const specialTitles = document.querySelectorAll('.special-title');
+  specialTitles.forEach(title => {
+    title.style.fontSize = '2rem';
+  });
 
   // Part 5
 
+const parentPastRaces = document.getElementById("past-races");
+//parent--ul
+const childLiChi = parentPastRaces.getElementsByTagName("li")[3]; //child with index for Chi-Town
+parentPastRaces.removeChild(childLiChi); //remove Chi-Town
+
 
   // Part 6
-
+const pastRacesList = document.querySelector('#past-races');
+let newCity = document.createElement("li"); //create new list element.
+let cityName = "Cincy"; // Set City Name
+newCity.textContent = cityName;
+pastRacesList.appendChild(newCity);
 
   // Part 7
+
+  const mainSection = document.querySelector('.main');
+  const newBlogPost = document.createElement('div');
+  newBlogPost.className = 'blog-post purple';
+  const newCityTitle = document.createElement('h1');
+  newCityTitle.textContent = cityName;
+  const newCityDescription = document.createElement('p');
+  newCityDescription.textContent = `Driving in ${cityName} feels like Nascar on Spirits!`;
+
+  newBlogPost.appendChild(newCityTitle);
+  newBlogPost.appendChild(newCityDescription);
+  mainSection.appendChild(newBlogPost);
 
 
   // Part 8
 
+  const quoteTitle = document.querySelector('#quote-title'); //Query select the #quote-titleID element
+  quoteTitle.addEventListener('click', randomQuote); //add event handler-----Test this out..
+
 
   // Part 9
+  const blogPosts = document.querySelectorAll('.blog-post'); // select all blog posts
+  blogPosts.forEach(blogPost => { //iterate through all the blog posts
+    blogPost.addEventListener('mouseout', function() {
+      blogPost.classList.toggle('purple'); //add mouseout purple class
+    });   
 
-
+    blogPost.addEventListener('mouseenter', function() {
+      blogPost.classList.toggle('red');
+     //add mouseenter to red class
+    });
+  });
 
 
 });
